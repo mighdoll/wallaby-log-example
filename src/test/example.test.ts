6ad2e3f851c8bc4fe43a9e2be5806077a75f244c
lee mighdoll <lee@underneath.ca>
2025-01-14 17:35:37 -0800

    isolating a minimal .conf for vitest.. this works

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,9 +1,5 @@
 import { test } from "vitest";
-import { dlog } from "berry-pretty";
-import { printTable } from "console-table-printer";
 
 test(`example`, () => {
-  dlog(`Hello, World!`);
-  //  printTable([{ a: 1, b: 2 }]); // also fails to appear in Wallaby log
   console.log("did I say hello?");
 });
