75b7745faabda6f8e3dbe71c6b7875bdf4e972c7
lee mighdoll <lee@underneath.ca>
2025-01-14 17:43:27 -0800

    fails 'may indicate a problem with vitest dependency conflicts'

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -9,14 +9,14 @@ const conf = {
     runner: "/opt/homebrew/Cellar/node/23.2.0/bin/node",
   },
   // why doesn't this work?
-  // testFramework: 'vitest' 
-  testFramework: {
-    version: "vitest@0.14.0",
-    configurator: "vitest@0.14.0",
-    reporter: "vitest@0.14.0",
-    starter: "vitest@0.14.0",
-    autoDetected: true,
-  },
+  testFramework: 'vitest' 
+  // testFramework: {
+  //   version: "vitest@0.14.0",
+  //   configurator: "vitest@0.14.0",
+  //   reporter: "vitest@0.14.0",
+  //   starter: "vitest@0.14.0",
+  //   autoDetected: true,
+  // },
 };
 
 export default conf;
